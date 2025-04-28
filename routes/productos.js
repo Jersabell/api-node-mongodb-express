700b3fd7220ce440c8c2363b93b58f98e1380c0e
Jersabell <jersabell@gmail.com>
2025-04-28 17:19:11 -0500

    token to products

--- routes/productos.js
+++ routes/productos.js
@@ -3,7 +3,7 @@ const route = express.Router();
 import productosController from '../controllers/productos.js' ;
 import { verificarToken } from '../helpers/autenticacion.js';
 
-route.post('/', productosController.create);
+route.post('/', verificarToken, productosController.create);
 route.get('/:id', productosController.getOne);
 route.get('/', productosController.getAll);
 route.put('/:id', verificarToken, productosController.update);
